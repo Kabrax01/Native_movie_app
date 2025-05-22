30d8417d5b4c5e64dc9a92aeb049eca025b1507d
Kabrax01 <rogalski.arnold@gmail.com>
2025-05-22 17:49:49 +0200

    chore: fixed app config

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare module "@env" {
+    export const MOVIE_API_KEY: string;
+    export const APPWRITE_PROJECT_ID: string;
+    export const APPWRITE_DATABASE_ID: string;
+    export const APPWRITE_COLLECTION_ID: string;
+}
